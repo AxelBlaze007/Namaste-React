bd7805451be45acb9194f9c718ab3808312cd7e1
AxelBlaze007 <roypannalal007@gmail.com>
2025-04-02 23:54:30 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "This is the H1 tag ")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+root.render(heading);
